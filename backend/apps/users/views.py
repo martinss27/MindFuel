35bb2dab153d9a1559fc9f54e56b864cb4f9deb9
martinss27 <leoalmeidamartins@gmail.com>
2025-07-03 00:05:25 -0300

    users views and urls

---
+++ backend/apps/users/views.py
@@ -0,0 +1,47 @@
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
+
+
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+
+    def post(self,request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            response = JsonResponse({
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                }
+            })
+            response.set_cookie(
+                key='refresh_token',
+                value=str(refresh),
+                httponly=True,
+                secure=False, 
+                samesite='Lax'
+            )
+            response.set_cookie( 
+                key='access_token',
+                value=str(refresh.access_token),
+                httponly=True,
+                secure=False,
+                samesite='Lax'
+            )
+            return response
+        return Response({'detail': 'invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
