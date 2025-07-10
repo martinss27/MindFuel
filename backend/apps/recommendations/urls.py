7d22aafb7f2e4862786803cc32cf16e07d1fc48a
martinss27 <leoalmeidamartins@gmail.com>
2025-07-09 23:10:01 -0300

    recommendations urls.py

--- backend/apps/recommendations/urls.py
+++ backend/apps/recommendations/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import GenerateRecommendationsView
-
+from .views import (
+    GenerateRecommendationsView,
+    UserHabitListCreateView,
+    UserHabitUpdateView,
+    UserHabitDeleteView,
+)
 urlpatterns = [
     path('generate/', GenerateRecommendationsView.as_view(), name='generate_recommendations'),
+    path('user-habits/', UserHabitListCreateView.as_view(), name='user_habit_list_create'),
+    path('user-habits/<int:pk>/', UserHabitUpdateView.as_view(), name='user_habit_update'),
+    path('user-habits/<int:pk>/delete/', UserHabitDeleteView.as_view(), name='user_habit_delete'),
 ]
